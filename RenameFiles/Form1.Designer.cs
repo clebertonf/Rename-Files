813bb9331a35ad5d819e319b9edc7c9ee5236a5c
Cleberton <clebertonfgc@gmail.com>
2025-02-20 19:17:59 -0300

    start project.

--- RenameFiles/Form1.Designer.cs
+++ RenameFiles/Form1.Designer.cs
@@ -1,38 +1,124 @@
-﻿namespace RenameFiles;
-
-partial class Form1
+﻿namespace RenameFiles
 {
-    /// <summary>
-    ///  Required designer variable.
-    /// </summary>
-    private System.ComponentModel.IContainer components = null;
-
-    /// <summary>
-    ///  Clean up any resources being used.
-    /// </summary>
-    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-    protected override void Dispose(bool disposing)
+    partial class Form1
     {
-        if (disposing && (components != null))
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button btnSelectSourceFolder;
+        private System.Windows.Forms.Label lblSourceFolder;
+        private System.Windows.Forms.Button btnSelectDestinationFolder;
+        private System.Windows.Forms.Label lblDestinationFolder;
+        private System.Windows.Forms.TextBox txtSubstring;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton rbtnCurrentFolder;
+        private System.Windows.Forms.RadioButton rbtnSelectFolder;
+        private System.Windows.Forms.Button btnRename;
+
+        protected override void Dispose(bool disposing)
         {
-            components.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
         }
-        base.Dispose(disposing);
+
+        private void InitializeComponent()
+        {
+            this.btnSelectSourceFolder = new System.Windows.Forms.Button();
+            this.lblSourceFolder = new System.Windows.Forms.Label();
+            this.btnSelectDestinationFolder = new System.Windows.Forms.Button();
+            this.lblDestinationFolder = new System.Windows.Forms.Label();
+            this.txtSubstring = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rbtnCurrentFolder = new System.Windows.Forms.RadioButton();
+            this.rbtnSelectFolder = new System.Windows.Forms.RadioButton();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            
+            this.btnSelectSourceFolder.Location = new System.Drawing.Point(12, 15);
+            this.btnSelectSourceFolder.Name = "btnSelectSourceFolder";
+            this.btnSelectSourceFolder.Size = new System.Drawing.Size(100, 30);
+            this.btnSelectSourceFolder.TabIndex = 0;
+            this.btnSelectSourceFolder.Text = "Select";
+            this.btnSelectSourceFolder.UseVisualStyleBackColor = true;
+            this.btnSelectSourceFolder.Click += new System.EventHandler(this.btnSelectSourceFolder_Click);
+            
+            this.lblSourceFolder.AutoSize = true;
+            this.lblSourceFolder.Location = new System.Drawing.Point(120, 22);
+            this.lblSourceFolder.Name = "lblSourceFolder";
+            this.lblSourceFolder.Size = new System.Drawing.Size(170, 20);
+            this.lblSourceFolder.TabIndex = 1;
+            this.lblSourceFolder.Text = "Folder: No folder selected";
+            
+            this.btnSelectDestinationFolder.Location = new System.Drawing.Point(12, 55);
+            this.btnSelectDestinationFolder.Name = "btnSelectDestinationFolder";
+            this.btnSelectDestinationFolder.Size = new System.Drawing.Size(100, 30);
+            this.btnSelectDestinationFolder.TabIndex = 2;
+            this.btnSelectDestinationFolder.Text = "Select";
+            this.btnSelectDestinationFolder.UseVisualStyleBackColor = true;
+            this.btnSelectDestinationFolder.Click += new System.EventHandler(this.btnSelectDestinationFolder_Click);
+            
+            this.lblDestinationFolder.AutoSize = true;
+            this.lblDestinationFolder.Location = new System.Drawing.Point(120, 62);
+            this.lblDestinationFolder.Name = "lblDestinationFolder";
+            this.lblDestinationFolder.Size = new System.Drawing.Size(180, 20);
+            this.lblDestinationFolder.TabIndex = 3;
+            this.lblDestinationFolder.Text = "Destination: No folder selected";
+            
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Substring to remove:";
+            
+            this.txtSubstring.Location = new System.Drawing.Point(12, 125);
+            this.txtSubstring.Name = "txtSubstring";
+            this.txtSubstring.Size = new System.Drawing.Size(280, 27);
+            this.txtSubstring.TabIndex = 5;
+            
+            this.rbtnCurrentFolder.AutoSize = true;
+            this.rbtnCurrentFolder.Location = new System.Drawing.Point(12, 165);
+            this.rbtnCurrentFolder.Name = "rbtnCurrentFolder";
+            this.rbtnCurrentFolder.Size = new System.Drawing.Size(140, 35);
+            this.rbtnCurrentFolder.TabIndex = 6;
+            this.rbtnCurrentFolder.TabStop = true;
+            this.rbtnCurrentFolder.Text = "Rename all files in the folder";
+            this.rbtnCurrentFolder.UseVisualStyleBackColor = true;
+            
+            this.rbtnSelectFolder.AutoSize = true;
+            this.rbtnSelectFolder.Location = new System.Drawing.Point(250, 165);
+            this.rbtnSelectFolder.Name = "rbtnSelectFolder";
+            this.rbtnSelectFolder.Size = new System.Drawing.Size(95, 30);
+            this.rbtnSelectFolder.TabIndex = 7;
+            this.rbtnSelectFolder.TabStop = true;
+            this.rbtnSelectFolder.Text = "Rename Selected Folder";
+            this.rbtnSelectFolder.UseVisualStyleBackColor = true;
+
+            this.btnRename.Location = new System.Drawing.Point(12, 200);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(120, 35);
+            this.btnRename.TabIndex = 8;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.rbtnSelectFolder);
+            this.Controls.Add(this.rbtnCurrentFolder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSubstring);
+            this.Controls.Add(this.lblDestinationFolder);
+            this.Controls.Add(this.btnSelectDestinationFolder);
+            this.Controls.Add(this.lblSourceFolder);
+            this.Controls.Add(this.btnSelectSourceFolder);
+            this.Name = "Form1";
+            this.Text = "File Renamer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
     }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
-    }
-
-    #endregion
 }
--- RenameFiles/Form1.cs
+++ RenameFiles/Form1.cs
@@ -1,9 +1,89 @@
-namespace RenameFiles;
-
-public partial class Form1 : Form
+namespace RenameFiles
 {
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
+        private string sourceFolder = "";
+        private string destinationFolder = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectSourceFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    sourceFolder = fbd.SelectedPath;
+                    lblSourceFolder.Text = "Folder: " + sourceFolder;
+                }
+            }
+        }
+
+        private void btnSelectDestinationFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    destinationFolder = fbd.SelectedPath;
+                    lblDestinationFolder.Text = "Destination: " + destinationFolder;
+                }
+            }
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtSubstring.Text))
+            {
+                MessageBox.Show("Please enter the substring to remove!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (rbtnCurrentFolder.Checked)
+            {
+                sourceFolder = Directory.GetCurrentDirectory();
+                destinationFolder = sourceFolder;
+            }
+            else if (rbtnSelectFolder.Checked)
+            {
+                if (string.IsNullOrWhiteSpace(sourceFolder) || string.IsNullOrWhiteSpace(destinationFolder))
+                {
+                    MessageBox.Show("Please select both source and destination folders!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+            }
+
+            try
+            {
+                string substringToRemove = txtSubstring.Text;
+                var files = Directory.GetFiles(sourceFolder)
+                                     .Where(f => Path.GetFileName(f).Contains(substringToRemove))
+                                     .ToList();
+
+                if (files.Count == 0)
+                {
+                    MessageBox.Show("No files found with the given substring.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return;
+                }
+
+                foreach (var file in files)
+                {
+                    string originalName = Path.GetFileName(file);
+                    string newName = originalName.Replace(substringToRemove, "").Trim();
+                    string newPath = Path.Combine(destinationFolder, newName);
+
+                    File.Move(file, newPath);
+                }
+
+                MessageBox.Show("Files renamed successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error renaming files: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
