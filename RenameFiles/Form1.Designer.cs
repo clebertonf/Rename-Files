48d4fc160b1b273257f85b61946881725962f039
Cleberton <clebertonfgc@gmail.com>
2025-03-02 14:50:09 -0300

    Version two with new start and end string optionality.

--- RenameFiles/Form1.Designer.cs
+++ RenameFiles/Form1.Designer.cs
@@ -1,104 +1,167 @@
-﻿using System;
-using System.Windows.Forms;
-
-namespace FileRenamer
+﻿namespace FileRenamer
 {
-    public partial class Form1 : Form
+    partial class Form1
     {
         private System.Windows.Forms.TextBox txtSourcePath;
         private System.Windows.Forms.Button btnSelectSource;
         private System.Windows.Forms.TextBox txtDestinationPath;
         private System.Windows.Forms.Button btnSelectDestination;
-        private System.Windows.Forms.TextBox txtTextToRemove;
-        private System.Windows.Forms.Button btnRenameFiles;
         private System.Windows.Forms.Label lblSource;
         private System.Windows.Forms.Label lblDestination;
-        private System.Windows.Forms.Label lblTextToRemove;
         
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPageSpecific;
+        private System.Windows.Forms.TabPage tabPageBetween;
+        
+        private System.Windows.Forms.TextBox txtTextToRemove;
+        private System.Windows.Forms.Label lblTextToRemove;
+        private System.Windows.Forms.Button btnRenameFiles;
+        
+        private System.Windows.Forms.TextBox txtInitialString;
+        private System.Windows.Forms.TextBox txtFinalString;
+        private System.Windows.Forms.Label lblInitialString;
+        private System.Windows.Forms.Label lblFinalString;
+        private System.Windows.Forms.Button btnRenameBetween;
+        
         private void InitializeComponent()
         {
+            this.lblSource = new System.Windows.Forms.Label();
             this.txtSourcePath = new System.Windows.Forms.TextBox();
             this.btnSelectSource = new System.Windows.Forms.Button();
+            this.lblDestination = new System.Windows.Forms.Label();
             this.txtDestinationPath = new System.Windows.Forms.TextBox();
             this.btnSelectDestination = new System.Windows.Forms.Button();
+            
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPageSpecific = new System.Windows.Forms.TabPage();
+            this.tabPageBetween = new System.Windows.Forms.TabPage();
+            
+            this.lblTextToRemove = new System.Windows.Forms.Label();
             this.txtTextToRemove = new System.Windows.Forms.TextBox();
             this.btnRenameFiles = new System.Windows.Forms.Button();
-            this.lblSource = new System.Windows.Forms.Label();
-            this.lblDestination = new System.Windows.Forms.Label();
-            this.lblTextToRemove = new System.Windows.Forms.Label();
-            this.SuspendLayout();
             
-            // Label Source
+            this.lblInitialString = new System.Windows.Forms.Label();
+            this.txtInitialString = new System.Windows.Forms.TextBox();
+            this.lblFinalString = new System.Windows.Forms.Label();
+            this.txtFinalString = new System.Windows.Forms.TextBox();
+            this.btnRenameBetween = new System.Windows.Forms.Button();
+
+            this.tabControl1.SuspendLayout();
+            this.tabPageSpecific.SuspendLayout();
+            this.tabPageBetween.SuspendLayout();
+            this.SuspendLayout();
+
             this.lblSource.AutoSize = true;
             this.lblSource.Location = new System.Drawing.Point(12, 15);
             this.lblSource.Name = "lblSource";
             this.lblSource.Size = new System.Drawing.Size(93, 15);
             this.lblSource.Text = "Pasta de Origem:";
-            
-            // txtSourcePath
+
             this.txtSourcePath.Location = new System.Drawing.Point(120, 12);
             this.txtSourcePath.Size = new System.Drawing.Size(300, 23);
-            
-            // btnSelectSource
+
             this.btnSelectSource.Location = new System.Drawing.Point(430, 12);
             this.btnSelectSource.Size = new System.Drawing.Size(75, 23);
             this.btnSelectSource.Text = "Selecionar";
             this.btnSelectSource.Click += new System.EventHandler(this.btnSelectSource_Click);
-            
-            // Label Destination
+
             this.lblDestination.AutoSize = true;
-            this.lblDestination.Location = new System.Drawing.Point(12, 50);
+            this.lblDestination.Location = new System.Drawing.Point(12, 45);
             this.lblDestination.Name = "lblDestination";
             this.lblDestination.Size = new System.Drawing.Size(102, 15);
             this.lblDestination.Text = "Pasta de Destino:";
-            
-            // txtDestinationPath
-            this.txtDestinationPath.Location = new System.Drawing.Point(120, 47);
+
+            this.txtDestinationPath.Location = new System.Drawing.Point(120, 42);
             this.txtDestinationPath.Size = new System.Drawing.Size(300, 23);
-            
-            // btnSelectDestination
-            this.btnSelectDestination.Location = new System.Drawing.Point(430, 47);
+
+            this.btnSelectDestination.Location = new System.Drawing.Point(430, 42);
             this.btnSelectDestination.Size = new System.Drawing.Size(75, 23);
             this.btnSelectDestination.Text = "Selecionar";
             this.btnSelectDestination.Click += new System.EventHandler(this.btnSelectDestination_Click);
-            
-            // Label TextToRemove
+
+            this.tabControl1.Controls.Add(this.tabPageSpecific);
+            this.tabControl1.Controls.Add(this.tabPageBetween);
+            this.tabControl1.Location = new System.Drawing.Point(12, 80);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(540, 220);
+
+            this.tabPageSpecific.Controls.Add(this.lblTextToRemove);
+            this.tabPageSpecific.Controls.Add(this.txtTextToRemove);
+            this.tabPageSpecific.Controls.Add(this.btnRenameFiles);
+            this.tabPageSpecific.Location = new System.Drawing.Point(4, 24);
+            this.tabPageSpecific.Name = "tabPageSpecific";
+            this.tabPageSpecific.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageSpecific.Size = new System.Drawing.Size(532, 192);
+            this.tabPageSpecific.Text = "Remover Texto Específico";
+            this.tabPageSpecific.UseVisualStyleBackColor = true;
+
             this.lblTextToRemove.AutoSize = true;
-            this.lblTextToRemove.Location = new System.Drawing.Point(12, 85);
+            this.lblTextToRemove.Location = new System.Drawing.Point(10, 15);
             this.lblTextToRemove.Name = "lblTextToRemove";
             this.lblTextToRemove.Size = new System.Drawing.Size(105, 15);
             this.lblTextToRemove.Text = "Remover texto:";
-            
-            // txtTextToRemove
-            this.txtTextToRemove.Location = new System.Drawing.Point(120, 82);
+
+            this.txtTextToRemove.Location = new System.Drawing.Point(120, 12);
             this.txtTextToRemove.Size = new System.Drawing.Size(300, 23);
-            
-            // btnRenameFiles
-            this.btnRenameFiles.Location = new System.Drawing.Point(12, 120);
-            this.btnRenameFiles.Size = new System.Drawing.Size(493, 30);
+
+            this.btnRenameFiles.Location = new System.Drawing.Point(10, 50);
+            this.btnRenameFiles.Size = new System.Drawing.Size(495, 30);
             this.btnRenameFiles.Text = "Renomear Arquivos";
             this.btnRenameFiles.Click += new System.EventHandler(this.btnRenameFiles_Click);
-            
-            // Form1
+
+            this.tabPageBetween.Controls.Add(this.lblInitialString);
+            this.tabPageBetween.Controls.Add(this.txtInitialString);
+            this.tabPageBetween.Controls.Add(this.lblFinalString);
+            this.tabPageBetween.Controls.Add(this.txtFinalString);
+            this.tabPageBetween.Controls.Add(this.btnRenameBetween);
+            this.tabPageBetween.Location = new System.Drawing.Point(4, 24);
+            this.tabPageBetween.Name = "tabPageBetween";
+            this.tabPageBetween.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageBetween.Size = new System.Drawing.Size(532, 192);
+            this.tabPageBetween.Text = "Remover Entre Strings";
+            this.tabPageBetween.UseVisualStyleBackColor = true;
+
+            this.lblInitialString.AutoSize = true;
+            this.lblInitialString.Location = new System.Drawing.Point(10, 15);
+            this.lblInitialString.Name = "lblInitialString";
+            this.lblInitialString.Size = new System.Drawing.Size(83, 15);
+            this.lblInitialString.Text = "String Inicial:";
+
+            this.txtInitialString.Location = new System.Drawing.Point(120, 12);
+            this.txtInitialString.Size = new System.Drawing.Size(300, 23);
+
+            this.lblFinalString.AutoSize = true;
+            this.lblFinalString.Location = new System.Drawing.Point(10, 45);
+            this.lblFinalString.Name = "lblFinalString";
+            this.lblFinalString.Size = new System.Drawing.Size(70, 15);
+            this.lblFinalString.Text = "String Final:";
+
+            this.txtFinalString.Location = new System.Drawing.Point(120, 42);
+            this.txtFinalString.Size = new System.Drawing.Size(300, 23);
+
+            this.btnRenameBetween.Location = new System.Drawing.Point(10, 80);
+            this.btnRenameBetween.Size = new System.Drawing.Size(495, 30);
+            this.btnRenameBetween.Text = "Renomear Arquivos";
+            this.btnRenameBetween.Click += new System.EventHandler(this.btnRenameBetween_Click);
+
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(520, 170);
+            this.ClientSize = new System.Drawing.Size(564, 250);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.lblSource);
             this.Controls.Add(this.txtSourcePath);
             this.Controls.Add(this.btnSelectSource);
             this.Controls.Add(this.lblDestination);
             this.Controls.Add(this.txtDestinationPath);
             this.Controls.Add(this.btnSelectDestination);
-            this.Controls.Add(this.lblTextToRemove);
-            this.Controls.Add(this.txtTextToRemove);
-            this.Controls.Add(this.btnRenameFiles);
             this.Name = "Form1";
             this.Text = "Renomeador de Arquivos";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPageSpecific.ResumeLayout(false);
+            this.tabPageSpecific.PerformLayout();
+            this.tabPageBetween.ResumeLayout(false);
+            this.tabPageBetween.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
--- RenameFiles/Form1.cs
+++ RenameFiles/Form1.cs
@@ -1,7 +1,14 @@
+using System.Text.RegularExpressions;
+
 namespace FileRenamer
 {
     public partial class Form1 : Form
     {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
         private void btnSelectSource_Click(object sender, EventArgs e)
         {
             using (FolderBrowserDialog dialog = new FolderBrowserDialog())
@@ -12,7 +19,7 @@ namespace FileRenamer
                 }
             }
         }
-
+        
         private void btnSelectDestination_Click(object sender, EventArgs e)
         {
             using (FolderBrowserDialog dialog = new FolderBrowserDialog())
@@ -23,44 +30,121 @@ namespace FileRenamer
                 }
             }
         }
-
+        
         private void btnRenameFiles_Click(object sender, EventArgs e)
         {
             string sourcePath = txtSourcePath.Text;
             string destinationPath = txtDestinationPath.Text;
             string textToRemove = txtTextToRemove.Text;
 
-            if (string.IsNullOrWhiteSpace(sourcePath) || string.IsNullOrWhiteSpace(destinationPath) || string.IsNullOrWhiteSpace(textToRemove))
+            if (string.IsNullOrWhiteSpace(sourcePath) ||
+                string.IsNullOrWhiteSpace(destinationPath) ||
+                string.IsNullOrWhiteSpace(textToRemove))
             {
-                MessageBox.Show("Por favor, preencha todos os campos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                MessageBox.Show("Por favor, preencha todos os campos.", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
 
             if (!Directory.Exists(sourcePath) || !Directory.Exists(destinationPath))
             {
-                MessageBox.Show("Uma ou ambas as pastas selecionadas não existem.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Uma ou ambas as pastas selecionadas não existem.", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
             try
             {
                 string[] files = Directory.GetFiles(sourcePath);
-                int count = 0;
+                int countChanged = 0;
 
                 foreach (string file in files)
                 {
                     string fileName = Path.GetFileName(file);
-                    string newFileName = fileName.Replace(textToRemove, "", StringComparison.OrdinalIgnoreCase);
+                    string newFileName = Regex.Replace(fileName, Regex.Escape(textToRemove), "", RegexOptions.IgnoreCase);
+                    
+                    if (string.Equals(fileName, newFileName, StringComparison.Ordinal))
+                        continue;
+                    
                     string newFilePath = Path.Combine(destinationPath, newFileName);
                     File.Copy(file, newFilePath);
-                    count++;
+                    countChanged++;
                 }
 
-                MessageBox.Show($"{count} arquivos renomeados com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if (countChanged == 0)
+                {
+                    MessageBox.Show("Nenhum arquivo foi alterado. Verifique se a string informada existe nos nomes dos arquivos.",
+                        "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    MessageBox.Show($"{countChanged} arquivos renomeados com sucesso!", "Sucesso",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show("Erro ao processar arquivos: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show("Erro ao processar arquivos: " + ex.Message, "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private void btnRenameBetween_Click(object sender, EventArgs e)
+        {
+            string sourcePath = txtSourcePath.Text;
+            string destinationPath = txtDestinationPath.Text;
+            string initialStr = txtInitialString.Text;
+            string finalStr = txtFinalString.Text;
+
+            if (string.IsNullOrWhiteSpace(sourcePath) ||
+                string.IsNullOrWhiteSpace(destinationPath) ||
+                string.IsNullOrWhiteSpace(initialStr) ||
+                string.IsNullOrWhiteSpace(finalStr))
+            {
+                MessageBox.Show("Por favor, preencha todos os campos.", "Aviso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!Directory.Exists(sourcePath) || !Directory.Exists(destinationPath))
+            {
+                MessageBox.Show("Uma ou ambas as pastas selecionadas não existem.", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                string[] files = Directory.GetFiles(sourcePath);
+                int countChanged = 0;
+                string pattern = Regex.Escape(initialStr) + ".*?" + Regex.Escape(finalStr);
+                foreach (string file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    string newFileName = Regex.Replace(fileName, pattern, "", RegexOptions.IgnoreCase);
+                    
+                    if (string.Equals(fileName, newFileName, StringComparison.Ordinal))
+                        continue;
+                    
+                    string newFilePath = Path.Combine(destinationPath, newFileName);
+                    File.Copy(file, newFilePath);
+                    countChanged++;
+                }
+                if (countChanged == 0)
+                {
+                    MessageBox.Show("Nenhum arquivo foi alterado. Verifique se as strings informadas existem nos nomes dos arquivos.",
+                        "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    MessageBox.Show($"{countChanged} arquivos renomeados com sucesso!", "Sucesso",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao processar arquivos: " + ex.Message, "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
